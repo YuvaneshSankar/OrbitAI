eaf812ad840c23a4aa2f218f3b9c00bfd3d5e8be
YuvaneshSankar <yuvanesh.skv@gmail.com>
2025-09-06 03:19:03 +0530

    Add files via upload

---
+++ agentic_rag/__init__.py
@@ -0,0 +1,10 @@
+from .newrag import rag_chat, DataCollector, RAGSystem
+
+__version__ = "1.0.0"
+__author__ = "OrbitAI Team"
+
+__all__ = [
+    "rag_chat",
+    "DataCollector", 
+    "RAGSystem"
+]
